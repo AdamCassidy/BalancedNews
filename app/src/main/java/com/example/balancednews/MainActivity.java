85a699d65626285f9afbedd0f094d72adf716557
Adam Cassidy <adamcassidy011@gmail.com>
2020-09-26 19:03:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/balancednews/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.balancednews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar)
+        TextView txtTitle = findViewById(R.id.txtTitle);
+        EditText editSearch = findViewById(R.id.editSearch);
+        editSearch.getText().toString()
+
+    }
+
+    public void onBtnClick(View view) {
+
+    }
+}
