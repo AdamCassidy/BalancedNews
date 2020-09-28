0adccc349ee1567c36d2aef45ac12669f9df0e32
Adam Cassidy <adamcassidy011@gmail.com>
2020-09-27 23:10:28 -0400

    Add Volley and boilerplate api call.
    To eventually use Android Volley to get news api JSON data.

--- app/src/main/java/com/example/balancednews/MainActivity.java
+++ app/src/main/java/com/example/balancednews/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.balancednews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView searchResults;
+    private String API_KEY = "placeholder";
+    private String TAG = "errorBalance";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClick(View view) {
-
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        String url = "http://newsapi.org/v2/top-headlines/apiKey=" + API_KEY;
+        JsonObjectRequest jsonObjectrequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                searchResults.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectrequest);
     }
 }
